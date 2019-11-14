299e01fbc16b97019c7f6dee63bc2c196824b88a
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-11-14 09:22:45 +0100

    LAB6Task1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const App = () => (
-  <h1>Minimal React</h1>  
-)
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            employee: [],
+            isLoading: false
+        }
+    }
+    componentDidMount() {
+        this.setState({isLoading: true})
+        fetch('http://localhost:3004/employees')
+            .then(response => response.json())
+            .then(data => this.setState({ employee: data }))
+            .then(()=>this.setState({isLoading: false}));
+    }
+
+    render() {
+        return (<div>
+            <h1>Minimal React</h1>
+            <h3>Lab6</h3>
+            Data: {this.state.isLoading ? "Loading..." : ""}
+            <hr/>
+            {this.state.employee.length}
+
+
+        </div>);
+    }
+}
 
 export default App
